a8cc5f6a00cd473899898c32f2c71fc0c7ab62d5
nmakes <nav.naveenvenkat@gmail.com>
2017-12-16 11:35:23 +0000

    adding partial algebra and network libraries

---
+++ algebra.py
@@ -0,0 +1,47 @@
+from math import sqrt, pow, exp, arctan, abs
+
+def mult(vec1, vec2):
+	
+	if len(vec1) != len(vec2):
+		print "mult(): ERROR!! VECTORS ARE OF DIFFERENT LENGTH. vec1 =", vec1, ", vec2 =", vec2
+		return None
+	else:
+		vec = []
+		for i in range(len(vec1)):
+			print i
+			vec.append(vec1[i] * vec2[i])
+
+		return vec
+
+
+def dot(vec1, vec2):
+	
+	Sum = 0.0
+
+	if len(vec1) != len(vec2):
+		print "dot(): ERROR!! VECTORS ARE OF DIFFERENT LENGTH. vec1 =", vec1, ", vec2 =", vec2
+		return None
+	else:
+		for i in range(len(vec1)):
+			Sum += vec1[i] * vec2[i]
+
+		return Sum
+
+
+def mod(vec, l_measure=2):
+
+	Sum = 0
+
+	if type(l_measure)==int:
+		
+		for i in vec:
+			Sum += pow(i,l_measure)
+
+		Sum = pow(i, 1/float(l_measure))
+
+		return Sum
+
+	else:
+
+		print "mod(): ERROR!! l_measure not identified. l_measure =", measure
+		return None
--- network.py
+++ network.py
@@ -1,1 +1,112 @@
-class 
+import algebra
+
+# Standard activation functions
+
+def identity(x):
+	return x
+
+def d_identity(x):
+	return 1
+
+def step(x):
+	if x<0:
+		return 0
+	else:
+		return 1
+
+def d_step(x):
+	if x!=0:
+		return 0
+	else:
+		return None
+
+def logistic(x):
+	return 1/(1+algebra.exp(-x))
+
+def d_logistic(x):
+	f= logistic(x)
+	return f * (1 - f)
+
+def tanh(x):
+	return (2.0/(1+exp(-2*x))) - 1.0
+
+def d_tanh(x):
+	f= tanh(x)
+	return 1-f*f
+
+def arctan(x):
+	return algebra.arctan(x)
+
+def d_arctan(x):
+	return 1.0/(1+x*x)
+
+def softsign(x):
+	return x / (1 + algebra.abs(x))
+
+def d_softsign(x):
+	return 1 / ((1 + algebra.abs(x)) ** 2)
+
+def isru(x,a=1):
+	return x / algebra.sqrt()
+
+standard_activation_functions = {
+	'identity': (lambda x: x, lambda x: 1),
+	'step': (lambda x: 0 if x<0 else 1, lambda x: 0 if x!=0 else None),
+	'logistic': (lambda x: 1/(1+algebra.exp(-x)), lambda x: (1/(1+algebra.exp(-x)))*(1- (1/(1+algebra.exp(-x))))),
+	'tanh': (lambda x: (1+algebra.exp(-2*x)))/(1+algebra.exp(-2*x)), 
+}
+
+class ActivationFunction:
+
+	def __init__(self, name=None, F=None, dF=None):
+		
+		if name == 'identity':
+
+		self.name = name
+		self.F = F
+		self.dF : dF,
+
+
+	def 
+
+class Neuron: # Describes the neuron
+
+	'''
+		Written by Naveen Venkat
+		nav.naveenvenkat@gmail.com
+		https://github.com/nmakes
+	'''
+
+	def __init__(self, weights=None, bias=None, activation=None):
+		self.weights = weights
+		self.bias = bias
+		self.activation = activation
+
+
+	# Update weights & bias
+	def update(self, dws, db):
+		self.weights -= dws
+		self.bias -= db
+
+
+	# Modify the neuron, if required in future
+	def modify(self, weights, bias, activation):
+		self.weights = weights
+		self.bias = bias
+		self.activation = activation		
+
+
+	def predict(self, inputs):
+
+
+
+class layer: # Describes a layer in the network
+
+	def __init__(self, neurons=None):
+		self.neurons = neurons
+
+
+
+class network:
+
+	def __init__(self, n_layers, )
